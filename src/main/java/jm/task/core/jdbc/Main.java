88a7b9dbbf12abe5fa5bec91c1d66a547a73b1e4
BoreBoreDoria <norcen@yandex.ru>
2020-07-20 18:24:35 +0500

    v6 Не работает HibernateSessionFactory

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,17 +1,21 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
+import jm.task.core.jdbc.dao.UsersEntity;
+import jm.task.core.jdbc.util.HibernateSessionFactory;
+import org.hibernate.Session;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        UserDaoJDBCImpl us = new UserDaoJDBCImpl();
-        us.dropUsersTable();
+        Session session = HibernateSessionFactory.getSessionFactory().openSession();
+        session.beginTransaction();
+        UsersEntity usersEntity = new UsersEntity();
+        usersEntity.setName("Aleks");
+        usersEntity.setLastName("Petrenko");
+        usersEntity.setAge(25);
+        session.save(usersEntity);
+        session.getTransaction().commit();
+        session.close();
     }
 }
---
+++ src/main/java/jm/task/core/jdbc/dao/UsersEntity.java
@@ -0,0 +1,76 @@
+package jm.task.core.jdbc.dao;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users", schema = "testbase")
+public class UsersEntity {
+    private int id;
+    private String name;
+    private String lastName;
+    private int age;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 45)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "lastName", nullable = false, length = 45)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "age", nullable = false)
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UsersEntity that = (UsersEntity) o;
+
+        if (id != that.id) return false;
+        if (age != that.age) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + age;
+        return result;
+    }
+}
